fcd0276fe356a794c80ed9218a8e9bdb2008043d
chllamas <chrisllamas2000@gmail.com>
2024-03-28 15:52:55 -0700

    Quick fix for args

--- better_ls.sh
+++ better_ls.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-ls -lF --color=always | grep --color=never '/$'
-ls -lF --color=always | grep --color=never -v '/$' | sed '1d'
+# Check if the first argument is provided
+if [ -n "$1" ]; then
+    # If provided, use the first argument
+    first_arg=$1
+else
+    # If not provided, use a default string
+    first_arg="."
+fi
+
+ls -lF --color=always "$first_arg" | grep --color=never '/$'
+ls -lF --color=always "$first_arg" | grep --color=never -v '/$' | sed '1d'
